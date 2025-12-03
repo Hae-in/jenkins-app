82e3add2892a9dccd14dfc237493498546672459
haein <93hilee@naver.com>
2025-12-03 10:35:26 +0900

    first commit

---
+++ src/main/java/org/example/jenkinsapp/JenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkinsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAppApplication.class, args);
+	}
+
+}
